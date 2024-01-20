6158224c55444db96a61bcc41dadaf073d6d7d72
Abdul Karim Tamim <116204905+abdultamim02@users.noreply.github.com>
2024-01-19 22:09:11 -0800

    APIKey.py

---
+++ APIKey.py
@@ -0,0 +1,11 @@
+def APIKey():
+    """
+    *IMPORTANT*
+    For optimal functionality of this project, please obtain your API Key from Fixer.io by following these steps:
+    1- Visit the link https://fixer.io/
+    2- Click on "SIGN UP FREE"
+    3- Choose the "Free $0" option and click on "Subscribe"
+    4- Create an account, and you will gain access to your free API Key.
+    """
+    API_Key = "PASTE_YOUR_API_KEY_HERE"
+    return API_Key
